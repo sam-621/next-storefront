aa9d5f5729984b3d1b6bfbbdf1c4b19ddc53e446
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-07-05 23:15:44 -0700

    feat(lib/cart):  add headless ui dialog to drawer

--- src/lib/common/components/drawer.tsx
+++ src/lib/common/components/drawer.tsx
@@ -1,77 +1,63 @@
 'use client';
 
-import { type FC, useEffect } from 'react';
+import { type FC } from 'react';
 
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
-import { cn } from '@/lib/common/utils';
-
 export const Drawer: FC<Props> = ({ isOpen, trigger, title, handleClose, footer, children }) => {
-  // Prevent scrolling when the drawer is open
-  // add 15px of padding (scrollbar width) to the body to prevent shifting content
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-      document.body.style.paddingRight = '15px';
-    } else {
-      document.body.style.overflow = 'auto';
-      document.body.style.paddingRight = '0px';
-    }
-  }, [isOpen]);
-
   return (
-    <div>
+    <>
       {trigger}
-      <div
-        className={cn(
-          'relative z-30 transition-opacity',
-          !isOpen && 'opacity-0 pointer-events-none'
-        )}
-        aria-labelledby="slide-over-title"
-        role="dialog"
-        aria-modal="true"
-      >
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <Dialog open={isOpen} onClose={handleClose} className="relative z-30">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity duration-300 ease-in-out data-[closed]:opacity-0"
+        />
 
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
-            <div
-              className={cn(
-                'pointer-events-none fixed inset-y-0 right-0 flex w-full md:w-fit md:pl-10 transition-transform',
-                !isOpen && 'translate-x-full'
-              )}
-            >
-              <div className="pointer-events-auto w-full md:w-screen md:max-w-md">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full">
+              <DialogPanel
+                transition
+                className="pointer-events-auto w-screen max-w-md transform transition duration-300 ease-in-out data-[closed]:translate-x-full sm:duration-300"
+              >
                 <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                   <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                     <div className="flex items-start justify-between">
-                      <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">
+                      <DialogTitle className="text-lg font-medium text-gray-900">
                         {title}
-                      </h2>
+                      </DialogTitle>
                       <div className="ml-3 flex h-7 items-center">
                         <button
                           type="button"
-                          className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
                           onClick={handleClose}
+                          className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
                         >
-                          <span className="absolute -inset-0.5"></span>
+                          <span className="absolute -inset-0.5" />
                           <span className="sr-only">Close panel</span>
-                          <XMarkIcon className="h-6 w-6" />
+                          <XMarkIcon aria-hidden="true" className="h-6 w-6" />
                         </button>
                       </div>
                     </div>
 
-                    {children}
+                    <div className="mt-8">
+                      <div className="flow-root">
+                        <ul role="list" className="-my-6 divide-y divide-gray-200">
+                          {children}
+                        </ul>
+                      </div>
+                    </div>
                   </div>
 
                   {footer}
                 </div>
-              </div>
+              </DialogPanel>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Dialog>
+    </>
   );
 };
 
