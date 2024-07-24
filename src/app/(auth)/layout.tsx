626db094886e78e442ed95f6c62dad0a578a37bc
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-07-23 23:47:29 -0700

    feat(auth):  add AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,9 +2,11 @@ import { type ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      {children}
+    <div className="grid grid-cols-1 h-screen md:grid-cols-2">
+      <section className="h-full max-w-[350px] mx-auto grid items-center">{children}</section>
+      <section className="hidden md:block relative">
+        <img src="/images/auth-form.png" alt="auth" className="w-full h-screen" />
+      </section>
     </div>
   );
 }
